1ef1c5d77240dfd79072ac3b4deb59341a21b2fa
Minh Le <duyminh.le310@gmail.com>
2022-08-19 16:47:47 -0400

    Refresh

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,15 @@
-import Head from 'next/head'
-
-import NavBar from '../Components/NavBar';
-import Title from '../Components/Title';
-import Testimonial from '../Components/Testimonial';
-import BestPartner from '../Components/BestPartner';
-import Gallery from '../Components/Gallery';
-import Services from '../Components/Services';
-
-import { Row, Col_25, Col_50, Col_75 } from '../LayoutComponents';
+import Head from "next/head";
 
 export default function Home() {
+
   return (
     <>
       <Head>
-        <title>My Design Collection</title>
+        <title>Gallery Collection</title>
       </Head>
-      <Row>
-        <Col_25>NayaHome</Col_25>
-        <Col_50>
-          <NavBar />
-        </Col_50>
-        <Col_25>
-          LET&apos;S TALK
-        </Col_25>
-      </Row>
-      <Row>
-        <Col_75>
-          <Title/>
-        </Col_75>
-        <Col_25>
-          <Testimonial/>
-        </Col_25>
-      </Row>
-      <Row>
-        <Col_25>
-          <BestPartner/>
-        </Col_25>
-        <Col_75>
-          <Gallery/>
-        </Col_75>
-      </Row>
-      <Row>
-        <Services/>
-      </Row>  
-    </>
-  )
-}
 
+      
+    </>
+    
+  );
+}
